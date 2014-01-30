163cb95ae64dfa3c41aa5dcfd00ec52199c1cc9f
Karen Etheridge <ether@cpan.org>
2014-01-30 05:08:23 +0000

    Import of ETHER/Acme-Tiny-0.003 from CPAN.
    gitpan-cpan-distribution: Acme-Tiny
    gitpan-cpan-version:      0.003
    gitpan-cpan-path:         ETHER/Acme-Tiny-0.003.tar.gz
    gitpan-cpan-author:       ETHER
    gitpan-cpan-maturity:     released

---
+++ README.md
@@ -0,0 +1,54 @@
+# NAME
+
+Acme::Tiny - minimal minimalism
+
+# VERSION
+
+version 0.003
+
+# SYNOPSIS
+
+    use Acme::Tiny;
+
+# DESCRIPTION
+
+This module does the tiniest thing of all -- nothing!
+
+Given all the recent releases of `*::Tiny` modules, I felt like I needed to
+get in on the action while there was still part of the namespace left.
+
+<div>
+    <div style="text-align:center">
+    <img src="https://raw.github.com/karenetheridge/Acme-Tiny/master/images/tiny-tim_lg.jpg" title="Tiny Tim" width="227" height="280"/>
+    <img src="https://raw.github.com/karenetheridge/Acme-Tiny/master/images/Elton_John_Tiny_Dancer.jpg" title="Tiny Dancer" width="276" height="280"/>
+    </div>
+</div>
+
+# SUPPORT
+
+Bugs may be submitted through [the RT bug tracker](https://rt.cpan.org/Public/Dist/Display.html?Name=Acme-Tiny)
+(or [bug-Acme-Tiny@rt.cpan.org](mailto:bug-Acme-Tiny@rt.cpan.org)).
+I am also usually active on irc, as 'ether' at `irc.perl.org`.
+
+# SEE ALSO
+
+There are many excellent (and legitimate) \*::Tiny modules out there,
+including:
+
+- [HTTP::Tiny](https://metacpan.org/pod/HTTP::Tiny)
+- [Try::Tiny](https://metacpan.org/pod/Try::Tiny)
+- [Role::Tiny](https://metacpan.org/pod/Role::Tiny)
+- [Capture::Tiny](https://metacpan.org/pod/Capture::Tiny)
+- [Type::Tiny](https://metacpan.org/pod/Type::Tiny)
+- [Module::Build::Tiny](https://metacpan.org/pod/Module::Build::Tiny)
+
+# AUTHOR
+
+Karen Etheridge <ether@cpan.org>
+
+# COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Karen Etheridge.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
--- lib/Acme/Tiny.pm
+++ lib/Acme/Tiny.pm
@@ -1,1 +1,96 @@
-package Acme::Tiny;$VERSION=.4
+use strict;
+use warnings;
+package Acme::Tiny;
+BEGIN {
+  $Acme::Tiny::AUTHORITY = 'cpan:ETHER';
+}
+# git description: v0.002-10-g95b536b
+$Acme::Tiny::VERSION = '0.003';
+# ABSTRACT: minimal minimalism
+# vim: set ts=8 sw=4 tw=78 et :
+
+'oh hai';
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=for :stopwords Karen Etheridge irc
+
+=head1 NAME
+
+Acme::Tiny - minimal minimalism
+
+=head1 VERSION
+
+version 0.003
+
+=head1 SYNOPSIS
+
+    use Acme::Tiny;
+
+=head1 DESCRIPTION
+
+This module does the tiniest thing of all -- nothing!
+
+Given all the recent releases of C<*::Tiny> modules, I felt like I needed to
+get in on the action while there was still part of the namespace left.
+
+=for html <div style="text-align:center">
+<img src="https://raw.github.com/karenetheridge/Acme-Tiny/master/images/tiny-tim_lg.jpg" title="Tiny Tim" width="227" height="280"/>
+<img src="https://raw.github.com/karenetheridge/Acme-Tiny/master/images/Elton_John_Tiny_Dancer.jpg" title="Tiny Dancer" width="276" height="280"/>
+</div>
+
+=head1 SUPPORT
+
+Bugs may be submitted through L<the RT bug tracker|https://rt.cpan.org/Public/Dist/Display.html?Name=Acme-Tiny>
+(or L<bug-Acme-Tiny@rt.cpan.org|mailto:bug-Acme-Tiny@rt.cpan.org>).
+I am also usually active on irc, as 'ether' at C<irc.perl.org>.
+
+=head1 SEE ALSO
+
+There are many excellent (and legitimate) *::Tiny modules out there,
+including:
+
+=over 4
+
+=item *
+
+L<HTTP::Tiny>
+
+=item *
+
+L<Try::Tiny>
+
+=item *
+
+L<Role::Tiny>
+
+=item *
+
+L<Capture::Tiny>
+
+=item *
+
+L<Type::Tiny>
+
+=item *
+
+L<Module::Build::Tiny>
+
+=back
+
+=head1 AUTHOR
+
+Karen Etheridge <ether@cpan.org>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Karen Etheridge.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
